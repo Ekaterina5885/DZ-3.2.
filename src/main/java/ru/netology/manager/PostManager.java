531fc264f3918d41fc72dfc096d386af7016189d
Ekaterina Kolesnikova <1005885@mail.ru>
2021-06-14 21:21:29 +0300

    added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostVK;
+
+public class PostManager {
+    private PostVK[] post;
+
+    public PostVK[] searchPost(int ownerId, String domain, String query, boolean owners_only, int count, int offset) {
+        return null;
+    }
+
+    public void deletePost(int ownerId, int postId) {
+    }
+}
+
